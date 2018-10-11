371066f60a73c80fae9deec44d4795cc757d31e8
chemyoo <chemyoo@foxmail.com>
2018-10-11 17:22:07 +0800

    test the function where use a picture which from network.
    Signed-off-by: chemyoo <chemyoo@foxmail.com>

--- src/pers/chemyoo/main/test/FaceApiTest.java
+++ src/pers/chemyoo/main/test/FaceApiTest.java
@@ -34,6 +34,8 @@ public class FaceApiTest {
         try {
         	map.put("image", Base64Utils.encode(data));
 			System.err.println(FaceDetect.detect(map));
+			// use picture from network.
+			System.err.println(FaceDetect.detectFromUrl("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1539259612132&di=be744356e89718019759f9dcdcac9447&imgtype=0&src=http%3A%2F%2Fimg10.3lian.com%2Fshow2015%2F5%2F98%2F64.jpg"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
