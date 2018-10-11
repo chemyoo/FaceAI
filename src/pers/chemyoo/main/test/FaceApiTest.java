01cf84ff59177ec18707733e43afd3eb1811509b
chemyoo <chemyoo@foxmail.com>
2018-10-11 16:58:30 +0800

    update comment.
    Signed-off-by: chemyoo <chemyoo@foxmail.com>

--- src/pers/chemyoo/main/test/FaceApiTest.java
+++ src/pers/chemyoo/main/test/FaceApiTest.java
@@ -19,8 +19,8 @@ public class FaceApiTest {
 	
 	public static void main(String[] args) throws IOException {
 		Map<String, Object> map = new HashMap<>();
-		// age 年龄；beauty 相貌评分；gender 性别； race 肤色；face_type 人脸类型；
-		// human: 真实人脸 cartoon: 卡通人脸；
+		// age 年龄；beauty 相貌评分；gender 性别； race 肤色；
+		// face_type 人脸类型，human: 真实人脸 cartoon: 卡通人脸；
         map.put("face_field", "age,beauty,gender,race,face_type");
         map.put("image_type", FaceDetect.BASE64);
         File file = FileSelectUtils.getFile();
